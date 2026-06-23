f8c8b3751698c947fdc44a790a3916f99f5b1ecf
NikmalWahid22 <muhamadnikmalwahid@gmail.com>
2026-06-23 09:54:25 +0700

    fix: connect to railway mysql via env vars

--- backend-api/app/Config/Database.php
+++ backend-api/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => getenv('MYSQLHOST') ?: 'localhost',
+        'username'     => getenv('MYSQLUSER') ?: '',
+        'password'     => getenv('MYSQLPASSWORD') ?: '',
+        'database'     => getenv('MYSQLDATABASE') ?: '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => (int) (getenv('MYSQLPORT') ?: 3306),
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
